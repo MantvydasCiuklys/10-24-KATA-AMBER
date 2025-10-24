8c1cab849528a10aa85a98a8f72420af647c751d
t.sipavicius <t.sipavicius@blisdigital.com>
2025-10-24 12:30:19 +0300

    testA

--- AFKata/Tasks/TaskA/Tadas/task.js
+++ AFKata/Tasks/TaskA/Tadas/task.js
@@ -2,6 +2,19 @@ const { runTaskASmokeTests } = require("../taskSmokeTests");
 
 function firstUniqChar(s) {
   // your code goes here
+  if (s.length === 1) return 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const unique = s[i];
+
+    for (let j = i + 1; j < s.length; j++) {
+      if (s[j] == unique) {
+        return i;
+      }
+    }
+  }
+
+  return -1;
 }
 
 function runSmokeTests() {
