aed8f06ec63cc42e78bb3c71401a4078d4985e30
arnas-marozas <arnasm.dev@gmail.com>
2025-10-24 14:19:55 +0300

    test

--- AFKata/Tasks/TaskC/Arnas/Solver.cs
+++ AFKata/Tasks/TaskC/Arnas/Solver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Contestants.Arnas.TaskC;
 
@@ -11,6 +12,47 @@ public static class Solver
         if (payload is null)
         {
             throw new ArgumentNullException(nameof(payload));
+        }
+
+        var stream = new MemoryStream();
+        Tuple<(byte, short)?, (byte, short)?> current = new(null, null);
+        for (int i = 0; i < payload.Length; i++)
+        {
+            if (current.Item1 is not null && current.Item2 is not null)
+            {
+                current = new(null, null);
+            }
+            var c = payload[i];
+            var index = GetIndex(c);
+            short count = 1;
+            while (i + count < payload.Length && payload[i + count] == c)
+            {
+                count++;
+            }
+            if (current.Item1 is null)
+            {
+                current = new(new(index, count), null);
+            }
+            else
+            {
+                current = new(current.Item1, new(index, count));
+            }
+            
+            if (current.Item1 is not null && current.Item2 is not null)
+            {
+                var firstBytePrefix = Convert.ToByte(current.Item1.Value.Item1 << 2);
+                var firstByteAppendix = Convert.ToByte(current.Item1.Value.Item2 >> 12);
+                var firstByte = (byte)(firstByteAppendix | firstByteAppendix);
+                var secondByte = Convert.ToByte(current.Item1.Value.Item2 << 4 >> 8);
+                var thirdBytePrefix = Convert.ToByte(current.Item1.Value.Item2 & 0x07);
+                var thirdByteAppendix = Convert.ToByte(current.Item2.Value.Item1 >> 2 << 4);
+                var thirdByte = (byte)(thirdBytePrefix | thirdByteAppendix);
+                var fourthBytePrefix= Convert.ToByte(current.Item2.Value.Item1 & 0x03);
+                var fourthByteAppendix = Convert.ToByte(current.Item2.Value.Item2 >> 8);
+                var fourthByte = (byte)(fourthBytePrefix | fourthByteAppendix);
+                var fifthByte = Convert.ToByte(current.Item2.Value.Item2 & 0xFF);
+                
+            }
         }
 
         // baseline implementation just returns the input; contestants should do better
@@ -26,6 +68,16 @@ public static class Solver
 
         // baseline implementation assumes identity encoding
         return encoded;
+    }
+    
+    private static byte GetIndex(char c) {
+        return c switch
+        {
+            >= 'A' and <= 'Z' => (byte)(c - 'A'),
+            >= 'a' and <= 'z' => (byte)(c - 'a' + 26),
+            >= '0' and <= '9' => (byte)(c - '0' + 52),
+            _ => byte.MaxValue
+        };
     }
 
     public static void RunSmokeTests()
