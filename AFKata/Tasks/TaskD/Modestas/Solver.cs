577f17a13f0408757af5517295bc437953c0639f
Modestas Kmieliauskas <m.kmieliauskas@blisdigital.com>
2025-10-24 14:47:39 +0300

    Initial version MK

--- AFKata/Tasks/TaskD/Modestas/Solver.cs
+++ AFKata/Tasks/TaskD/Modestas/Solver.cs
@@ -6,6 +6,7 @@ public static class Solver
 {
     private const string SampleMessage = "Meet me at the old bridge at midnight.";
 
+        public static int cipher = 4;
     public static string Cipher(string plaintext)
     {
         if (plaintext is null)
@@ -13,13 +14,36 @@ public static class Solver
             throw new ArgumentNullException(nameof(plaintext));
         }
 
-        // TODO: replace with your custom cipher implementation
-        return plaintext;
+        string cypheredText = "";
+        for (int i = 0; i < plaintext.Length; i++)
+        {
+            cypheredText += Convert.ToChar(Convert.ToByte(plaintext[i]) + (cipher * cipher));
+        }
+
+        return cypheredText;
     }
 
-    public static void ShowCase()
+    public static string Decipher(string secretText)
+    {
+        if (secretText is null)
+        {
+            throw new ArgumentNullException(nameof(secretText));
+        }
+
+        string plainText = "";
+        for (int i = 0; i < secretText.Length; i++)
+        {
+            plainText += Convert.ToChar(Convert.ToByte(secretText[i]) - (cipher * cipher));
+        }
+
+        // TODO: replace with your custom cipher implementation
+        return plainText;
+    }
+
+    public static void RunSmokeTests()
     {
         var encoded = Cipher(SampleMessage);
+        var plain = Decipher(encoded);
         Console.WriteLine($"Sample input: {SampleMessage}");
         Console.WriteLine($"Your cipher output: {encoded}");
     }
