ebb98bfd74a9f3776295be756ad3702aa3ca6a6e
t.sipavicius <t.sipavicius@blisdigital.com>
2025-10-24 12:34:15 +0300

    ye boi

--- AFKata/Tasks/TaskA/Tadas/task.js
+++ AFKata/Tasks/TaskA/Tadas/task.js
@@ -4,14 +4,22 @@ function firstUniqChar(s) {
   // your code goes here
   if (s.length === 1) return 0;
 
+  let foundedChards = [];
   for (let i = 0; i < s.length; i++) {
-    const unique = s[i];
+    let foundDublicate = false;
+    let unique = s[i];
 
     for (let j = i + 1; j < s.length; j++) {
       if (s[j] == unique) {
-        return i;
+        foundDublicate = true;
       }
     }
+
+    if (!foundDublicate && foundedChards.indexOf(unique) === -1) {
+      return i;
+    }
+
+    foundedChards.push(unique);
   }
 
   return -1;
