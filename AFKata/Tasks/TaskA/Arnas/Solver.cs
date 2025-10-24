1e18a031ab3696a8bf7f617b5b31592407733023
arnas-marozas <arnasm.dev@gmail.com>
2025-10-24 12:25:22 +0300

    yolo

--- AFKata/Tasks/TaskA/Arnas/Solver.cs
+++ AFKata/Tasks/TaskA/Arnas/Solver.cs
@@ -6,8 +6,32 @@ public static class Solver
 {
     public static int? FirstUniqChar(string s)
     {
-        // your code goes here
-        return null;
+        if (s.Length == 0) return -1;
+        var dict = new Dictionary<char, Option>();
+        foreach (var c in s)
+        {
+            var value = dict.GetValueOrDefault(c);
+            if (value is null)
+            {
+                dict.Add(c, new Option { Index = dict.Count, Count = 1 });
+            }
+            else
+            {
+                value.Count++;
+            }
+        }
+        foreach (var val in dict.Where(val => val.Value.Count == 1))
+        {
+            return val.Value.Index;
+        }
+
+        return -1;
+    }
+
+    public class Option
+    {
+        public int Index { get; set; }
+        public int Count { get; set; }
     }
 
     public static void RunSmokeTests() => TaskASmokeTestHarness.Run(FirstUniqChar);
