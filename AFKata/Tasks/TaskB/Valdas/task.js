c54ebcb562f8f5cf5369f9147a6328e50a652330
Valdas Germanauskas <vgrmmail@gmail.com>
2025-10-24 12:59:28 +0300

    valdas-taskb

--- AFKata/Tasks/TaskB/Valdas/task.js
+++ AFKata/Tasks/TaskB/Valdas/task.js
@@ -1,8 +1,25 @@
 const { runTaskBSmokeTests } = require("../taskSmokeTests");
 
 function countDivisibleSubarrays(nums, k) {
+
+let a = 0;
+let count = 0;
+
+for (let i = 0; i < nums.length; i++) {
+  //
+    if (nums[i] % k === 0) {
+      for (let j = i; j < nums.length; j++) {
+        if (nums[j] % k !== 0) {
+          break;
+        }
+        count++;
+      }
+      a+=nums[i];
+    }
+  }
+
   // your code goes here
-  return -1;
+  return count;
 }
 
 function runSmokeTests() {
